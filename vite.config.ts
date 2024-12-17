376f816cb2f7820384b926516c02ce7668c8ee00
Eduard Kangash <kangash1996@gmail.com>
2024-12-17 12:33:28 +0300

    Last commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,10 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
-    },
-  },
-  root: '.',
-  build: {
-    outDir: path.join(__dirname, "./dist"),
+    }
   }
 })
